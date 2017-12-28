1c1426fc69d32dfc9c1d1c0e3bfb5ddc376866bf
Drew Leonard <andy.j.leonard@gmail.com>
2017-12-28 21:28:48 +0000

    Corrections to make it actually work

--- __init__.py
+++ __init__.py
@@ -1,19 +1,21 @@
 import logging
 
 from opsdroid.matchers import match_always, match_crontab
+from opsdroid.message import Message
+
 
 def setup(opsdroid):
     logging.debug("Loaded reminder module")
 
 
 @match_always
-def last_speaker(opsdroid, config, message):
+async def last_speaker(opsdroid, config, message):
     # Keep track of the last person in the room to speak.
     await opsdroid.memory.put("last_speaker", message.user)
 
 
 @match_crontab('* * * * *', timezone="Europe/London")
-def remind(opsdroid, config, message):
+async def remind(opsdroid, config, message):
     # Get the default connector
     connector = opsdroid.default_connector
     logging.debug(f"Selected {} as default connector")
@@ -28,4 +30,4 @@ def remind(opsdroid, config, message):
     last_speaker = await opsdroid.memory.get("last_speaker")
 
     # Remind the player
-    await message.respond(f"Hey {last_user}, don't forget to tell the DM you're finished.")
+    await message.respond(f"Hey {last_speaker}, don't forget to tell the DM you're finished.")
