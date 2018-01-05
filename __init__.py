a5e342056034997719877b6fcf5bff2177b66cb3
Drew Leonard <andy.j.leonard@gmail.com>
2018-01-05 10:17:59 +0000

    Add automation of reminders from config entries

--- __init__.py
+++ __init__.py
@@ -1,11 +1,34 @@
 import logging
+from functools import partial
 
 from opsdroid.matchers import match_always, match_crontab
 from opsdroid.message import Message
 
 
+async def reminder(opsdroid, config, message, specs):
+    # Get the default connector
+    connector = opsdroid.default_connector
+
+    # Get the default room for that connector
+    room = connector.default_room
+
+    # Create an empty message to respond to
+    message = Message("", None, room, connector)
+
+    # Remind the player
+    await message.respond(specs['message'])
+
+
 def setup(opsdroid):
     logging.debug("Loaded reminder module")
+    for skill in opsdroid.config['skills']:
+        if skill['name'] == 'remind':
+            for (funcname, specs) in skill['reminders'].items():
+                logging.debug(f"Creating reminder {funcname} for {specs['crontime']}.")
+                this_reminder = partial(reminder, specs=specs)
+                cron_decorator = match_crontab(specs['crontime'],
+                                               timezone="Europe/London")
+                this_reminder = cron_decorator(this_reminder)
 
 
 @match_always
